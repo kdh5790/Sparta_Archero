7703fd4e66636da70d1a74e6d811e578de439196
kdh5790 <k_0106@naver.com>
2025-02-21 22:55:20 +0900

    [ Chore ] 스킬 오브젝트 변수명 통일
    1. 2. 3. -> 1_ 2_ 3_

--- Assets/Scripts/Player/Skill/SkillManager.cs
+++ Assets/Scripts/Player/Skill/SkillManager.cs
@@ -6,11 +6,33 @@ public class SkillManager : MonoBehaviour
 {
     public static SkillManager instance;
 
-    public List<SkillInfo> skillInfoList;
+    public List<SkillInfo> skillTable;
+
 
     private void Awake()
     {
         if (instance == null)
             instance = this;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SkillInfo randSkill = GetRandomSkill();
+            Debug.Log($"{randSkill.name} Ω∫≈≥¿ª »πµÊ«ﬂΩ¿¥œ¥Ÿ.");
+        }
+    }
+
+    public SkillInfo GetRandomSkill()
+    {
+        int randomNum = Random.Range(0, skillTable.Count);
+
+        SkillInfo skill = skillTable[randomNum];
+
+        if (!skill.IsReacquirable)
+            skillTable.RemoveAt(randomNum);
+
+        return skill;
     }
 }
