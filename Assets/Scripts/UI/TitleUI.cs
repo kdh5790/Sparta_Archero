2b95923c8f89b736b8f84a178ff2d3e9f2dee261
buildthan <a9175226@gmail.com>
2025-02-24 14:48:22 +0900

    [ Feat ] 버튼 기능 활성화
    Start 버튼과 Exit 버튼 사용가능

--- Assets/Scripts/UI/TitleUI.cs
+++ Assets/Scripts/UI/TitleUI.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TitleUI : BaseUI
 {
+    Button startBtn;
+    Button exitBtn;
+
     protected override UIState GetUIState()
     {
         return UIState.Title;
@@ -11,6 +15,23 @@ public class TitleUI : BaseUI
 
     public override void Init(UIManager uiManager)
     {
-        base.Init(uiManager);
+        base.Init(uiManager); //ui manager ½ÇÇà
+
+        startBtn = transform.Find("StartButton").GetComponent<Button>();
+        exitBtn = transform.Find("ExitButton").GetComponent<Button>();
+
+        startBtn.onClick.AddListener(OnClickStartButton);
+        exitBtn.onClick.AddListener(OnClickExitButton);
+    }
+
+
+    public void OnClickStartButton()
+    {
+        uiManager.OnClickStart();
+    }
+
+    public void OnClickExitButton()
+    {
+        uiManager.OnClickExit();
     }
 }
