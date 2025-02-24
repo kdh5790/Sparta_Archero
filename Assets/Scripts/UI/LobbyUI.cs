b75c31d1559c98aa7475de57cdb334776adaf589
buildthan <a9175226@gmail.com>
2025-02-24 16:41:11 +0900

    [ Feat ] 스테이지 선택 기능 활성화
    어떤 스테이지를 선택했는지 알 수 있습니다

--- Assets/Scripts/UI/LobbyUI.cs
+++ Assets/Scripts/UI/LobbyUI.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 public class LobbyUI : BaseUI
 {
     Button stageStartBtn;
+    Button nextStageBtn;
+    Button prevStageBtn;
     protected override UIState GetUIState()
     {
         return UIState.Lobby;
@@ -15,6 +17,12 @@ public class LobbyUI : BaseUI
     {
         stageStartBtn = transform.Find("StageStartButton").GetComponent<Button>(); //스테이지 스타트 버튼 할당
         stageStartBtn.onClick.AddListener(OnClickStageStartButton); //스테이지 스타트 버튼을 누른 경우
+        
+        nextStageBtn = transform.Find("NextStageButton").GetComponent<Button>(); //다음 스테이지  버튼 할당
+        nextStageBtn.onClick.AddListener(OnClickNextStageButton);
+
+        prevStageBtn = transform.Find("PrevStageButton").GetComponent<Button>(); //이전 스테이지 버튼 할당
+        prevStageBtn.onClick.AddListener(OnClickPrevStageButton);
 
         base.Init(uiManager);
     }
@@ -22,6 +30,16 @@ public class LobbyUI : BaseUI
     public void OnClickStageStartButton()
     {
         uiManager.OnClickStageStart();
+    }
+
+    public void OnClickNextStageButton()
+    {
+        uiManager.OnClickNextStage();
+    }
+
+    public void OnClickPrevStageButton()
+    {
+        uiManager.OnClickPrevStage();
     }
 
 }
--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -2,17 +2,22 @@ using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SocialPlatforms.Impl;
 using UnityEngine.SceneManagement;
+using Unity.VisualScripting;
 
 public enum UIState
 {
-    Title,
-    Lobby
+    Title, //0
+    Lobby //1
 }
 
 public enum StageState
 {
-    Stage1,
-    Stage2
+    Min = -1, 
+
+    Stage1, //0
+    Stage2, //1
+
+    Max //enum StageState의 개수
 }
 
 public class UIManager : MonoBehaviour
@@ -23,6 +28,10 @@ public class UIManager : MonoBehaviour
     TitleUI titleUI = null;
 
     LobbyUI lobbyUI = null;
+
+    GameObject stage1 = null;
+
+    GameObject stage2 = null;
 
     static UIManager instance;
     public static UIManager Instance
@@ -42,6 +51,9 @@ public class UIManager : MonoBehaviour
         lobbyUI = GetComponentInChildren<LobbyUI>(true);
         lobbyUI?.Init(this);
 
+        stage1 = transform.Find("LobbyUI").transform.Find("Stage1").gameObject; //stage1과 stage2 ui 오브젝트를 찾아줘서 할당
+        stage2 = transform.Find("LobbyUI").transform.Find("Stage2").gameObject;
+
 
         ChangeState(UIState.Title);
     }
@@ -57,6 +69,19 @@ public class UIManager : MonoBehaviour
     public void ChangeStageState(StageState state) //아래에서 해당하는 stage를 찾아서 on off 해줌
     {
         stageState = state; //해당하는 stage를 찾아서 값을 넣어줌
+
+
+        switch(stageState)
+        {
+            case StageState.Stage1: //현재 스테이지가 스테이지1인 경우
+                stage1.SetActive(true);
+                stage2.SetActive(false);
+                break;
+            case StageState.Stage2: //현재 스테이지가 스테이지2인 
+                stage1.SetActive(false);
+                stage2.SetActive(true);
+                break;
+        }
     }
 
 
@@ -82,7 +107,24 @@ public class UIManager : MonoBehaviour
 
     public void OnClickStageStart() // 스테이지 실행 버튼을 누렀을 시
     {
+        //스테이지가 추가 된다면 이부분을 수정할 것
         SceneManager.LoadScene("MainScene");
+    }
+
+    public void OnClickNextStage()
+    {
+        if(stageState < StageState.Max - 1) //현재스테이지가 최대 스테이지 개수보다 적은 경우에만
+        {
+            ChangeStageState(stageState + 1); //다음 스테이지로
+        }    
+    }
+
+    public void OnClickPrevStage()
+    {
+        if (stageState > StageState.Min + 1) //현재스테이지가 최소 스태이지보다 큰 경우에만
+        {
+            ChangeStageState(stageState - 1); //이전 스테이지로
+        }
     }
 
 }
