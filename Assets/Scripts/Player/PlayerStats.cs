5d562e64ce2fa2602bfe8354e4831760d46c9785
kdh5790 <k_0106@naver.com>
2025-02-24 16:28:12 +0900

    [ Revert ] 코드 복구 작업

--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -32,4 +32,87 @@ public class PlayerStats : MonoBehaviour
     {
         level = 1; currentExp = 0; maxExp = 100; attackPower = 30; currentHealth = 200; maxHealth = 200; speed = 3f; attackSpeed = 1f;
     }
+<<<<<<< HEAD
 }
+=======
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            OnDamaged(30);
+        }
+    }
+
+    public void OnDamaged(int damage)
+    {
+        if (IsInvincivility)
+        {
+            Debug.Log($"¹«Àû »óÅÂÀÌ¹Ç·Î {damage}ÀÇ µ¥¹ÌÁö¸¦ ÀÔÁö ¾Ê½À´Ï´Ù.");
+            return;
+        }
+
+        if (Random.Range(0, 100) < DodgeChance)
+        {
+            Debug.Log("È¸ÇÇ¿¡ ¼º°øÇß½À´Ï´Ù.");
+            return;
+        }
+
+        currentHealth -= damage;
+        currentHealth = Mathf.Max(0, currentHealth);
+
+        if (currentHealth <= 0)
+        {
+            PlayerDead();
+        }
+
+        StartCoroutine(ApplyInvincibilityAfterDamage());
+    }
+
+    public void PlayerDead()
+    {
+        Debug.Log("ÇÃ·¹ÀÌ¾î »ç¸Á");
+    }
+
+    // µ¥¹ÌÁö¸¦ ÀÔÀº ÈÄ ¹«ÀûÆÇÁ¤
+    public IEnumerator ApplyInvincibilityAfterDamage()
+    {
+        IsInvincivility = true;
+
+        int count = 3; // ±ôºýÀÏ È½¼ö
+
+        for (int i = 0; i < count; i++)
+        {
+            sprite.color = new Color(sprite.color.r, sprite.color.g, sprite.color.b, 0.2f);
+            yield return new WaitForSeconds(0.1f);
+
+            sprite.color = new Color(sprite.color.r, sprite.color.g, sprite.color.b, 1f);
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        sprite.color = new Color(sprite.color.r, sprite.color.g, sprite.color.b, 1f);
+        IsInvincivility = false;
+    }
+
+    // ¹«Àû ½ºÅ³ º¸À¯ ½Ã 10ÃÊ ¸¶´Ù 2ÃÊ°£ ¹«Àû
+    public IEnumerator ApplyInvincibilitySkill()
+    {
+        while (currentHealth > 0)
+        {
+            IsInvincivility = false;
+
+            yield return new WaitForSeconds(10f);
+
+            Debug.Log("¹«Àû Àû¿ë");
+            IsInvincivility = true;
+            sprite.color = new Color(0.25f, 0.35f, 1);
+
+            yield return new WaitForSeconds(2f);
+
+            Debug.Log("¹«Àû ÇØÁ¦");
+            IsInvincivility = false;
+            sprite.color = Color.white;
+        }
+    }
+}
+>>>>>>> parent of 57a8298 ([ Chore ] í…ŒìŠ¤íŠ¸ ì½”ë“œ ì‚­ì œ)
