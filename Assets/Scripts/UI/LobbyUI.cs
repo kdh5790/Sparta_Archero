b5ce1a90885ae9a9c39b56529a864aaab33610f9
kdh5790 <k_0106@naver.com>
2025-02-26 16:45:09 +0900

    Revert "[ Feat ] 색상 변경 UI 기초 작업 완료"
    This reverts commit 0c838f47d6dd5058449579887c9c389b8cda071b.

--- Assets/Scripts/UI/LobbyUI.cs
+++ Assets/Scripts/UI/LobbyUI.cs
@@ -8,8 +8,6 @@ public class LobbyUI : BaseUI
     Button dungeonStartBtn;
     Button nextDungeonBtn;
     Button prevDungeonBtn;
-    Button ChangeColorBtn;
-
     protected override UIState GetUIState()
     {
         return UIState.Lobby;
@@ -25,9 +23,6 @@ public class LobbyUI : BaseUI
 
         prevDungeonBtn = transform.Find("PrevDungeonButton").GetComponent<Button>(); //이전 스테이지 버튼 할당
         prevDungeonBtn.onClick.AddListener(OnClickPrevDungeonButton);
-
-        ChangeColorBtn = transform.Find("ChangeColorButton").GetComponent<Button>(); // 색상 변경 버튼 할당
-        ChangeColorBtn.onClick.AddListener(OnClickChangeColorButton);
 
         base.Init(uiManager);
 
@@ -46,11 +41,6 @@ public class LobbyUI : BaseUI
     public void OnClickPrevDungeonButton()
     {
         uiManager.OnClickPrevDungeon();
-    }
-
-    public void OnClickChangeColorButton()
-    {
-        uiManager.OnClickChangeColor();
     }
 
 }
