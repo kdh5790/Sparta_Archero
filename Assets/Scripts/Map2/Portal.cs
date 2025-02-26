c8fc99574f073c93814544c67efaa1d1d0108562
greenmetamong <youngjaeda@naver.com>
2025-02-26 13:56:01 +0900

    [Feat]1-1~4stage hierarchy창, script 수정

--- Assets/Scripts/Map2/Portal.cs
+++ Assets/Scripts/Map2/Portal.cs
@@ -6,6 +6,9 @@ using UnityEngine.SceneManagement;
 public class Portal : MonoBehaviour
 {
     private string[] stageOrder = {
+        "Stage_1-1", "Stage_1-2", "Stage_1-3", "Stage_1-4",
+        "Stage_2-Box","Stage_3-1", "Stage_3-2", "Stage_3-3", "Stage_3-4",
+        "Stage_4-Boss1", 
         "Stage_5-1", "Stage_5-2", "Stage_5-3", "Stage_5-4",
         "Stage_6-Box", "Stage_7-1", "Stage_7-2", "Stage_7-3", "Stage_7-4",
         "Stage_8-Boss2"
