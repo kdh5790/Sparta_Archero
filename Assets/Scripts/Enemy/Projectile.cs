18eba6f52e0481d47a233d62ab347de08b959268
HYOSIN219 <38967913+HYOSIN219@users.noreply.github.com>
2025-02-27 11:21:44 +0900

    [Docs, Remove] Enemy 주석 추가, 안 쓰는 스크립트 삭제

--- Assets/Scripts/Enemy/Projectile.cs
+++ Assets/Scripts/Enemy/Projectile.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class Projectile : MonoBehaviour
+public class Projectile : MonoBehaviour     //몬스터 투사체
 {
     public int damage = 50; // 기본 데미지
 
@@ -12,20 +12,20 @@ public class Projectile : MonoBehaviour
         }
     }
 
-    void OnTriggerEnter2D(Collider2D collision)
+    void OnTriggerEnter2D(Collider2D collision)     //플레이어와 충돌시
     {
 
-        if (collision.CompareTag("Player"))
+        if (collision.CompareTag("Player"))     //태그확인
         {
             PlayerStats playerStats = collision.GetComponent<PlayerStats>();
 
             if (playerStats != null)
             {
-                Debug.Log($" 플레이어에게 {damage} 데미지");
+                Debug.Log($" 플레이어에게 {damage} 데미지"); //데미지 주기
                 playerStats.OnDamaged(damage);
             }
 
-            Destroy(gameObject);
+            Destroy(gameObject);        //투사체 파괴
         }
     }
 }
